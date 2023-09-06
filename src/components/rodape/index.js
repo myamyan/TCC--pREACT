bba04cf849b1bc7e261c6cdf37a888e53d83315b
-_Mya_- <129542768+myamyan@users.noreply.github.com>
2023-09-06 18:36:56 -0300

    Add files via upload

--- src/components/rodape/index.js
+++ src/components/rodape/index.js
@@ -4,64 +4,75 @@ import './index.scss';
 export default function Rodape () {
   return (
     <div className="rodape">
-
+<div className="container">
             <div className='esquerda2'>
 
            
-            <img className="logo" src="/assets/images/planta.png" alt="" />
+            {/* <img className="logo" src="/assets/images/planta.png" alt="" /> */}
 
-               <p>ATELIÊ RÉGIA</p>
-
-            <h1>O Ateliê Régia é uma empresa desenvolvida e liderada por mulheres com objetivo de peças bonitas, confortáveis e ecológicas.</h1>
-
+               <h1>ATELIÊ RÉGIA</h1>
+            <p className="paragrafo-esquerda2">O Ateliê Régia é uma empresa </p>
+            <p>desenvolvida e liderada por mulheres com objetivo de peças bonitas, confortáveis e ecológicas.</p>
+<div className="segura">
             <img className="rece" src="/assets/images/receiclo.png" alt="" />
 
             <img className="seguro" src="/assets/images/site.png" alt="" />
+            </div>
+            </div>
 
+            <div className="linha"></div>
+           
         <div className='redes-sociais'>
 
-            <p>redes sociais</p>
+            <h1>REDES SOCIAIS</h1>
 
-        <div className='insta'>
+ 
+<div className="icon-redes">
+<img className="i" src="/assets/images/insta.png" alt="" />  
+        <img className="i" src="/assets/images/insta.png" alt="" />
+        <img className="i" src="/assets/images/insta.png" alt="" />
+</div>
+      <div className="redes">
+     
+     <a href="">LOGIN</a>
+     <a href="">CADASTRO</a>
+     <div clas></div>
+     <div className="aaa">
+     <a href="">INSTRUÇÕES</a>
+     <p>Cuidados com as roupas</p>
+     </div>
+     <div> <a href="">Contato:</a>
+        <p>Fale conosco</p>  </div>
+    
+      </div>
 
-        <img className="seguro" src="/assets/images/insta.png" alt="" />
-        </div>
-
-        <div className='tt'>
-        <img className="seguro" src="/assets/images/tt.png" alt="" />
-        </div>
-
-        <div className='faic'>
-        <img className="seguro" src="/assets/images/facebook.png" alt="" />
-        </div>
-
-        <a>LOGIN</a>
-        <a>CADASTRO</a>
-        <a>INSTRUÇÕES:</a>
-
-        <h1>cuidados com as roupas</h1>   
-
-        <a>CONTATO</a>
-        <h1>fale conosco</h1>     
+         
 
         </div>
 
     <div className='suporte'>
-        <p>suporte</p>
-
-    <a>termos de uso</a>
-    <a>troca e devoluçõa</a>
-    <a>política de devoluçõa</a>
-    <a>politica de privasidade</a>
-
-
-    <h1>FORMAS DE PAGAMENTO</h1>
+        <h1>SUPORTE</h1>
+<div className="politicas">
+<a>Termos de uso</a>
+    <a>Troca e devoluçõa</a>
+    <a>Política de devoluçõa</a>
+    <a>Politica de privasidade</a>
+    <div className="formas">
+    <h2>FORMAS DE PAGAMENTO</h2>
     <img className="pagamentos" src="/assets/images/image 288.png" alt="" />
-
+    </div>
+  
+</div>
+  
     </div>
 
-      </div>
+      
 
+</div>
+<hr/>
+<div className="details">
+  <p>©2023 ATELIÊ RÉGIA - CNPJ 14341564000100/XXX-XX</p>
+</div>
          </div>
   );
-}
+} 
